dbbdcf4b33076b16d45a234e7a86c999c624ecfb
Vikram Sah <vikramabhishek11@gmail.com>
2022-10-08 17:17:42 -0500

    etesss

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,125 +1,125 @@
+import React from 'react';
 import { useState } from "react";
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import styled from "styled-components";
 
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+  margin-top: 20px;
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  padding: 50px;
+  flex: 1;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 30px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 5px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
-    const Container = styled.div`
-        width:100%;
-        display:flex;
-        height:100vh;
-        position:relative;
-        overflow:hidden;
-    `
-    const Arrow = styled.div`
-        width:50px;
-        height:50px;
-        background-color: #fff7f7;
-        border-radius:50%;
-        display:flex;
-        align-items:center;
-        justify-content:center;
-        position:absolute;
-        margin:auto;
-        top:0;
-        bottom:0;
-        left:${props=>props.direction === 'left' && '10px'};
-        right:${props=>props.direction === 'right' && '10px'};
-        margin:auto;
-        cursor:pointer;
-        opacity:0.5;
-        z-index:2;
-    `
-    const Wrapper = styled.div`
-        height:100%;
-        display:flex;
-        transition: all 1.5s ease;
-        transform:translateX(${props=>props.slideIndex * -100}vw);
-    `
-    const Slide =styled.div`
-        width:100vw;
-        height:100vh; 
-        display:flex;
-        align-items:center;
-        background-color: ${props=>props.bg};
-    `
-    const ImgContainer =styled.div`
-        height:100%;
-        flex:1;
-    `
-    const Image = styled.img`
-        margin-top:20px;
-        height:80%;
-    `
-    const InfoContainer =styled.div`
-        padding:50px;
-        flex:1;
-    `
-    const Title = styled.h1`
-        font-size:70px;
-    `
-    const Desc= styled.p`
-        margin: 30px 0px;
-        font-size: 20px;
-        font-weight: 500;
-        letter-spacing:3px;
-    `
-    const Button = styled.button`
-        padding:5px;
-        font-size:20px;
-        background-color:transparent;
-        cursor:pointer;
-    `
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const sliderItems = [
+    {
+      id: 0,
+      img: "/img/solo3-pdp-p02.png.large.2x.png",
+      title: "TOP PICKS CURATED ONLY FOR YOU",
+      desc: "Dont Compromise on Music! Get Flat 30% off for New Arrivals",
+      bg: "coral",
+    },
+    {
+      id: 1,
+      img: "/img/solo3-pdp-p02.png.large.2x copy.png",
+      title: "MIDTERM SALES!",
+      desc: "Dont Compromise on Music! Get Flat 30% off for New Arrivals",
+      bg: "#FFC700",
+    },
+  ];
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 1);
+    } else {
+      setSlideIndex(slideIndex < 1 ? slideIndex + 1 : 0);
+    }
+  };
 
-    const Slider = () => {
-        const [slideIndex, setSlideIndex] = useState(0);
-        const sliderItems = [
-            {
-                id:0,
-                img: '/img/solo3-pdp-p02.png.large.2x.png',
-                title: 'TOP PICKS CURATED ONLY FOR YOU',
-                desc: 'Dont Compromise on Music! Get Flat 30% off for New Arrivals',
-                bg: 'coral',
-            },
-            {
-                id:1,
-                img: '/img/solo3-pdp-p02.png.large.2x copy.png',
-                title: 'MIDTERM SALES!',
-                desc: 'Dont Compromise on Music! Get Flat 30% off for New Arrivals',
-                bg: '#FFC700',
-            }
-        ]
-        const handleClick=(direction)=>{
-            if(direction==='left'){
-                setSlideIndex(slideIndex>0?slideIndex-1: 1);
-            }else{
-                setSlideIndex(slideIndex<1?slideIndex+1: 0);
-            }
-        }
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOP NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
 
-    return ( 
-        <Container>
-            <Arrow direction='left' onClick={()=>handleClick('left')}>
-                <ArrowLeftOutlined/>
-            </Arrow>
-            <Wrapper slideIndex = {slideIndex}>
-                {sliderItems.map((item)=>(
-                    <Slide bg={item.bg} key={item.id}>
-                        <ImgContainer>
-                            <Image src={item.img}/>
-                        </ImgContainer>
-                        <InfoContainer>
-                            <Title>{item.title}</Title>
-                            <Desc>{item.desc}</Desc>
-                            <Button>SHOP NOW</Button>
-                        </InfoContainer>
-                    </Slide>
-                ))}
-            </Wrapper>
-            <Arrow direction='right' onClick={()=>handleClick('right')}> 
-                <ArrowRightOutlined/>
-            </Arrow>
-        </Container>
-     );
-}
- 
 export default Slider;
