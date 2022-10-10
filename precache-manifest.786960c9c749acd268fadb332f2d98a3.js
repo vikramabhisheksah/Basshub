e0b1a139b5965ff9da3d08e627bab057ddc19469
Vikram Sah <vikramabhishek11@gmail.com>
2022-10-09 20:50:27 -0500

    Updates

---
+++ precache-manifest.786960c9c749acd268fadb332f2d98a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebc87414949cc1eb6ab3",
+    "url": "/Basshub/static/css/main.5de55568.chunk.css"
+  },
+  {
+    "revision": "ebc87414949cc1eb6ab3",
+    "url": "/Basshub/static/js/main.ebc87414.chunk.js"
+  },
+  {
+    "revision": "c9ee7b98d52e11defc06",
+    "url": "/Basshub/static/css/1.ad0c6b60.chunk.css"
+  },
+  {
+    "revision": "c9ee7b98d52e11defc06",
+    "url": "/Basshub/static/js/1.c9ee7b98.chunk.js"
+  },
+  {
+    "revision": "12c47e2c53119e6725aa",
+    "url": "/Basshub/static/js/runtime~main.12c47e2c.js"
+  },
+  {
+    "revision": "e516efec486a552824147ae0e3f0ac2f",
+    "url": "/Basshub/index.html"
+  }
+];
